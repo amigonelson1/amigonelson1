afcaf85f3280ab210ed0b7d7f2d79e7a0f33c080
Nelson Amaris <amigonelson1@hotmail.com>
2022-10-27 17:31:20 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './index.css'
+import 'primeicons/primeicons.css';
+import { MemoPresentacion } from './components/Presentacion';
+//import confetti from 'https://cdn.skypack.dev/canvas-confetti';
+
 
 function App() {
+
+  const [changeState, setChangeState] = useState(1)
+
+  const Next = () => {
+    setChangeState(changeState => changeState + 1)
+    if (changeState > 2) { setChangeState(1) }
+  }
+
+  const Prev = () => {
+    setChangeState(changeState => changeState - 1)
+    if (changeState < 2) { setChangeState(3) }
+  }
+
+  /* funcion para agg confeti en nuestra web
+  confetti();
+*/
+
+  /*comparar un valor con varios usando el método .includes
+  const CompararValores = () => {
+    let valor = 5 //valor a comparar 
+    let a = 0 // valores de referencia (b , c, d...)
+    if ([a, "b", "c", "d"].includes(valor)) { console.log("resultado") }
+  }*/
+
+  /* Para evitar usar useEffect al cambiar un id de un componente se recomienda usar una key unica en el componente, eje:
+  <User key={id}/> al actualizar key nos aseguramos que al cambiar este el componente se actualice solo  
+  */
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div><h1>José Nelson Amaris Ortiz</h1></div>
+      <div className='menuPrincipal'>
+        <div className='item' onClick={e => setChangeState(1)}>¿Quien soy?</div>
+        <div className='item' onClick={e => setChangeState(2)}>Estudios</div>
+        <div className='item' onClick={e => setChangeState(3)}>Experiencia y habilidades</div>
+      </div>
+      <div className='containerFlex'>
+        <MemoPresentacion />
+        <div className='menuSecundario'>
+          <div className='item' onClick={e => setChangeState(1)}>¿Quien soy?</div>
+          <div className='item' onClick={e => setChangeState(2)}>Estudios</div>
+          <div className='item' onClick={e => setChangeState(3)}>Experiencia y habilidades</div>
+        </div>
+        <div className='container'>
+          {changeState === 1 ?
+            <div className='animar'>
+              <h2>¿Quien soy?</h2>
+              <p>
+                Nacido en Bucaramanga en 1981, criado, educado y rodeado de familiares y personas que me han servido de ejemplos
+                a seguir y con el valioso apoyo siempre de mi madre, quien ha estado ahí de manera incondicional y que me inculco los
+                valores del respeto, la honestidad y la responsabilidad.<br />
+                En la juventud abandoné mis estudios tecnológicos por dedicarme a laborar, cosa que mucho más adelante corregí (a mis 35 años)
+                retomando o más bien reiniciando la vida académica gracias al esfuerzo, dedicación y apoyo de mi madre y a la fortuna de poder
+                ingresar a tan prestigiosa y de la cual me siento muy orgulloso, mi alma mater UIS, alcanzando así una meta más en la vida, que es
+                la de ser un Ingeniero de Sistemas. <br />
+                Por un largo periodo de tiempo me desempeñé como técnico, área en la cual aprendí bastante y me fue muy útil la interacción
+                diaria con la gran cantidad de usuarios con los cuales trate.<br />
+                Respeto mucho la naturaleza y siento gran afinidad por los animales domésticos.<br />
+                Me considero curioso y autodidacta, también empático y paciente.
+
+              </p>
+            </div> : null}
+          {changeState === 2 ?
+            <div className='animar'>
+              <h2>Estudios</h2>
+              <ul>
+                <li>Graduado de primaria en 1992.</li>
+                <li>Bachiller en la modalidad académico humanidades en 1998.</li>
+                <li>Tecnólogo en electrónica (4 semestres) no culminado.</li>
+                <li>Técnico en electrónica graduado en 2005.</li>
+                <li>Ingeniero de Sistemas 2022.</li>
+              </ul>
+              <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            </div> : null}
+          {changeState === 3 ?
+            <div className='animar'>
+              <h2>Experiencia y habilidades</h2>
+              <p>Mi trayectoria laboral ha sido de la siguiente manera:</p>
+              <ul>
+                <li>Mi primer trabajo fue Botones en turno dominical.</li>
+                <li>Ayudante de venta en punto de pizza.</li>
+                <li>Todero y reemplazo en vacaciones en el área hotelera.</li>
+                <li>Técnico en instalación y mantenimiento redes domiciliarias HFC.</li>
+                <li>Técnico en mantenimiento en redes domiciliarias de cobre.</li>
+                <li>Técnico en manteniendo en redes externas HFC.</li>
+                <li>Técnico en instalación y mantenimiento en sistema de tv satelital.</li>
+                <li>Desarrollo Frontend usando Reactjs (como proyecto de grado).</li>
+              </ul>
+              <p>Dentro de mis fortalezas cabe destacar el constante aprendizaje de manera autodidacta, la consulta e
+                indagación de temas que considero relevantes y que en algún momento puedan ser útiles.
+              </p>
+            </div> : null}
+          <div onClick={Prev} className='flechaAtras'>
+            <i className="pi pi-chevron-left"></i>
+          </div>
+          <div onClick={Next} className='flechaAdelante'>
+            <i className="pi pi-chevron-right"></i>
+          </div>
+        </div>
+      </div>
+      <div className='socialMedia'>
+        <div className='socialmedia'>
+          <a href='https://walink.co/f6015f' target="_blank" rel="noopener noreferrer">
+            <i className="pi pi-whatsapp icons" />
+          </a>
+          <a href='https://www.linkedin.com/in/jos%C3%A9-nelson-amaris-ortiz-037166202/' target="_blank" rel="noopener noreferrer">
+            <i className="pi pi-linkedin icons" />
+          </a>
+          <a href='https://github.com/amigonelson1' target="_blank" rel="noopener noreferrer">
+            <i className="pi pi-github icons" />
+          </a>
+          <a href='https://www.facebook.com/jose.nelson.9047/' target="_blank" rel="noopener noreferrer">
+            <i className="pi pi-facebook icons" />
+          </a>
+          <a href='https://www.instagram.com/invites/contact/?i=1qx0tzam84mim&utm_content=3m2xau3' target="_blank" rel="noopener noreferrer">
+            <i className="pi pi-instagram icons" />
+          </a>
+          <a href='mailto:amigonelson1@hotmail.com?Subject=Ví%20tu%20web%20personal,%20deseo%20contactarte' target="_blank" rel="noopener noreferrer">
+            <i className="pi pi-envelope icons" />
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Presentacion.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import miFoto1 from '../images/1.png'
+import miFoto2 from '../images/2.jpg'
+
+export const Presentacion = () => {
+
+    const control = [miFoto1, miFoto2]
+    const [fotos, setFotos] = useState(0)
+
+    // Funcion para estar cambiando de imágen
+
+    useEffect(() => {
+        let controlArray = 0
+        let totalLista = control.length
+        const intervalId = setInterval(() => {
+            if (controlArray >= totalLista - 1) { controlArray = -1 }
+            setFotos(controlArray = controlArray + 1);
+        }, 16000); // Este tiempo va de la mano con el css para evitar conflicto
+        return () => clearInterval(intervalId);
+    }, [control.length]);
+
+    //hacemos un componente interno para garantizar el uso de los estilos al renderizar
+    const Presentation = () => {
+        return (
+            <div className='containerImg'>
+                <img src={control[fotos]} alt='Imagen de perfil' className='imgRound' />
+            </div>
+        )
+    }
+
+
+    // Retornamos el componente
+    return (
+        <div>
+            <Presentation />
+        </div>
+    )
+}
+
+// Exportamos el componente envuelto en React.memo para evitar que se renderice cuando el padre haga un cambio
+export const MemoPresentacion = React.memo(Presentacion);
